a5db086d37bd3cfb15ba6808ad5d30e360e601d0
Luciano Ratamero <luciano@ratamero.com>
2024-12-26 14:04:10 -0300

    Migrate error page to app/state

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,13 @@
-<script>
+<script lang="ts">
 	import { dev } from '$app/environment';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 
 	const offline = typeof navigator !== 'undefined' && navigator.onLine === false;
 
-	let message = $state(offline ? 'Find the internet and try again' : $page.error?.message);
+	let message = $state(offline ? 'Find the internet and try again' : page.error?.message);
 
-	let title = $state(offline ? 'Offline' : $page.status);
-	if ($page.status === 404) {
+	let title = $state(offline ? 'Offline' : page.status);
+	if (page.status === 404) {
 		title = 'Page not found :(';
 		message = 'Sorry! If you think this URL is broken, please let me know!';
 	}
@@ -17,40 +17,41 @@
 	<title>{title}</title>
 </svelte:head>
 
-<section class='container prose mx-auto py-12 dark:prose-invert'>
-	<h1>{$page.status}: {title}</h1>
+<section class="container prose mx-auto py-12 dark:prose-invert">
+	<h1>{page.status}: {title}</h1>
 
-	{#if $page.status === 404}
-		<p class=''>There is no post at the slug <code>{$page.url.pathname}</code>.</p>
-		<p><a href={'/ideas/?filter=' + $page.url.pathname.slice(1)}>Try searching for it here!</a></p>
-		<p class=''>If you believe this was a bug, please let me know! Email luciano [at] ratamero.com</p>
+	{#if page.status === 404}
+		<p class="">There is no post at the slug <code>{page.url.pathname}</code>.</p>
+		<p class="">
+			If you believe this was a bug, please let me know! Email luciano [at] ratamero.com
+		</p>
 	{:else}
-		<p class='font-mono'>{message}</p>
+		<p class="font-mono">{message}</p>
 	{/if}
-	{#if dev && $page.error.message}
-		<pre class='mono overflow-scroll bg-zinc-800 p-8'>{$page.error.message}</pre>
+	{#if dev && page.error.message}
+		<pre class="mono overflow-scroll bg-zinc-800 p-8">{page.error.message}</pre>
 	{/if}
 </section>
 
 <style>
-    h1,
-    p {
-        margin: 0 auto;
-    }
+	h1,
+	p {
+		margin: 0 auto;
+	}
 
-    h1 {
-        font-size: 2.8em;
-        font-weight: 700;
-        margin: 0 0 0.5em 0;
-    }
+	h1 {
+		font-size: 2.8em;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
 
-    p {
-        margin: 1em auto;
-    }
+	p {
+		margin: 1em auto;
+	}
 
-    @media (min-width: 480px) {
-        h1 {
-            font-size: 4em;
-        }
-    }
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
 </style>
