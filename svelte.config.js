23a82fcddc577abea682cee1399f18d63731fddb
Luciano Ratamero <luciano@ratamero.com>
2024-12-26 13:45:57 -0300

    Reconfigure mdsvex to render markdown files properly

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,50 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from 'mdsvex';
-import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeFigure from 'rehype-figure';
+import rehypeSlug from 'rehype-slug';
+import rehypeStringify from 'rehype-stringify';
+import gfm from 'remark-gfm';
+import parse from 'remark-parse';
+import remarkToc from 'remark-toc';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 // mdsvex config
 const mdsvexConfig = {
 	extensions: ['.svelte.md', '.md', '.svx'],
 	layout: {
-		_: './src/mdsvexlayout.svelte' // default mdsvex layout
+		_: './src/lib/BlogLayout.svelte' // default mdsvex layout
 	},
-	remarkPlugins: [],
+	remarkPlugins: [
+		[
+			remarkToc,
+			{
+				heading: 'Table of Contents|Resumo|toc',
+				tight: true
+			}
+		]
+	],
 	rehypePlugins: [
+		parse,
+		gfm,
 		rehypeSlug,
+		rehypeFigure,
 		[
 			rehypeAutolinkHeadings,
 			{
 				behavior: 'wrap'
 			}
-		]
+		],
+		rehypeStringify
 	]
 };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.html', '.svx', ...mdsvexConfig.extensions],
+	extensions: ['.svelte', '.md', '.html', '.svx', ...mdsvexConfig.extensions],
 	preprocess: [
 		mdsvex(mdsvexConfig),
-		preprocess({
+		sveltePreprocess({
 			postcss: true
 		})
 	],
