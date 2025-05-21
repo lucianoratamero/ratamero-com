e54db83354f6aea3fe48d14892c55c69f7fd5032
Luciano Ratamero <luciano@ratamero.com>
2025-05-21 18:25:18 -0300

    Fix stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { mdsvex } from 'mdsvex';
+import { dirname, join } from 'path';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeFigure from 'rehype-figure';
 import rehypeSlug from 'rehype-slug';
@@ -8,13 +9,17 @@ import gfm from 'remark-gfm';
 import parse from 'remark-parse';
 import remarkToc from 'remark-toc';
 import { sveltePreprocess } from 'svelte-preprocess';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // mdsvex config
 const mdsvexConfig = {
 	extensions: ['.svelte.md', '.md', '.svx'],
 	layout: {
-		plain: './src/lib/PlainLayout.svelte',
-		_: './src/lib/BlogLayout.svelte' // default mdsvex layout
+		plain: join(__dirname, '/src/lib/PlainLayout.svelte'),
+		_: join(__dirname, '/src/lib/BlogLayout.svelte') // default mdsvex layout
 	},
 	remarkPlugins: [
 		[
