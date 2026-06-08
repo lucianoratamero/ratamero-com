6d2c85d98cfb3e936a3d2d7d672a0822c68fb483
Luciano Ratamero <luciano@ratamero.com>
2026-06-07 23:46:44 -0300

    Fix typescript issues

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,18 @@
 interface String {
 	replaceAll(search: string, replace: string): string;
 }
+
+declare module 'highlight.js/lib/languages/*' {
+	import type { LanguageFn } from 'highlight.js';
+	const lang: LanguageFn;
+	export default lang;
+}
+
+declare module 'js-yaml' {
+	export function load(str: string): unknown;
+}
+
+declare module 'lodash-es' {
+	import lodash from 'lodash';
+	export = lodash;
+}
